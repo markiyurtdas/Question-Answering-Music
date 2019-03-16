13258449d2ee5cd6af35406c2bd6b7df2e18487e
Erdem Çalışır <i.erdemcalisir@gmail.com>
2019-03-16 22:02:04 +0300

    Create ArtistQuestionHelper.java

---
+++ QA erdem deneme regex/src/utils/ArtistQuestionHelper.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utils;
+
+import model.Artist;
+
+
+
+/**
+ *
+ * @author marki
+ */
+public class ArtistQuestionHelper {
+    private Artist artist;
+    
+    public ArtistQuestionHelper() {
+    }
+    
+    public String AskQuestion(String string){
+        if(string.equalsIgnoreCase("name")){
+            return artist.getArtistName();
+        }else if(string.equalsIgnoreCase("followers")){
+            return      ""+ artist.getFollowersTotal();
+        }else if(string.equalsIgnoreCase("href")){
+            return artist.getHref();
+        }else if(string.equalsIgnoreCase("id")){
+            return ""+artist.getSpotifyId();
+        }else if(string.equalsIgnoreCase("popularity")){
+            return ""+artist.getPopularity();
+        }else if(string.equalsIgnoreCase("type")){
+            return artist.getType();
+        }else{
+            return "Hata. Yazdığınız soruyu kontrol edin !!";
+        }
+        
+        
+    }
+
+    public Artist getArtist() {
+        return artist;
+    }
+
+    public void setArtist(Artist artist) {
+        this.artist = artist;
+    }
+    
+    
+    
+    
+    
+    
+}
