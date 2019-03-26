0e841709fba243b8c97c8e53a6e8e6b7d13357d6
Erdem Çalışır <i.erdemcalisir@gmail.com>
2019-03-26 17:19:35 +0300

    Update ArtistQuestionHelper.java

--- Qa/src/utils/ArtistQuestionHelper.java
+++ Qa/src/utils/ArtistQuestionHelper.java
@@ -37,9 +37,9 @@ public class ArtistQuestionHelper {
         boolean isMatch= false;
         int i=0;
         while (i<artistName.size()){
-           mPattern = Pattern.compile("[kim[dir]]*" + "[\\s]?" + artistName.get(i)+ "[\\s]?" + "[kim[dir]]*" + "[?]*",Pattern.CASE_INSENSITIVE);
+           //mPattern = Pattern.compile("[kim[dir]]*" + "[\\s]?" + artistName.get(i)+ "[\\s]?" + "[kim[dir]]*" + "[?]*",Pattern.CASE_INSENSITIVE);
 
-           // mPattern = Pattern.compile("[[\\w]*[^\\w]*]*" + artistName.get(i).toUpperCase()+ "[[\\w]*[^\\w]*]*" ,Pattern.CASE_INSENSITIVE);
+           mPattern = Pattern.compile("[[\\w]*[^\\w]*]*" + artistName.get(i)+ "[[\\w]*[^\\w]*]*" ,Pattern.CASE_INSENSITIVE);
 
             mMatcher = mPattern.matcher(input);
 
@@ -47,7 +47,7 @@ public class ArtistQuestionHelper {
              isMatch = mMatcher.matches();
             if (isMatch == true)
             {
-                queriedArtistList.add(artistler.get(i));
+                mPattern2 = Pattern.compile("[[\\w]*[^\\w]*]*" + "adres" + "[[\\w]*[^\\w]*]*");
             }
 
         i++;
